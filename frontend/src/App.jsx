2572daeee7367ba1610372e64e34dc832748f617
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-04-28 19:09:53 +0300

    I created the header part and included the bootstrap icon part.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
-
   return (
-    <>
+    <div>
+      <Header/>
       <h1>
         Hello
       </h1>
-    </>
+    </div>
   )
 }
 

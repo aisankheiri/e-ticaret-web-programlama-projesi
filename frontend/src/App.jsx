d1ac8ddae7e1826b0c4c17975f942cd09d6af621
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-05 13:53:11 +0300

    Footer section added to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Header from './components/Header'
+import Footer from "./components/Footer.jsx";
+import Policy from './components/Policy.jsx';
 
 function App() {
 
   return (
     <div>
-      <Header/>
-      <h1>
-        Hello
-      </h1>
+        <Header/>
+          <h1>
+            Hello
+          </h1>
+        <Policy/>
+        <Footer/>
     </div>
   )
 }

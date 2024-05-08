dbd1c40b6aa8b0f2669e51be7dab08df3dbe9024
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-08 19:26:12 +0300

    category section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
 import Header from './components/Layout/Header/Header.jsx'
 import Footer from "./components/Layout/Footer/Footer.jsx";
 import Policy from './components/Layout/Policy/Policy.jsx';
 import Slider from './components/Layout/Slider/Slider.jsx';
 import Categories from './components/Layout/Categories/Categories.jsx';
+import Products from './components/Products/Products.jsx';
+import './App.css'
 
 
 
@@ -14,6 +15,7 @@ function App() {
         <Header/>
         <Slider/>
         <Categories/>
+        <Products/>
         <Policy/>
         <Footer/>
     </div>

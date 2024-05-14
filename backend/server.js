06565dd63ea27b756431845ad46ba24687125a77
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-14 23:45:20 +0300

    a lot

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const app = express();
+const port = 5000;
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello Express.js");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Bu api routessss");
+});
+
+app.listen(5000, () => {
+  connect();
+  console.log(`sunucu ${5000} portunda cslisiyor`);
+});

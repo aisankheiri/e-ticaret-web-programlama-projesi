c73184b6bb44220836fbca8536317ae34d7cb020
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-15 14:59:30 +0300

    Categories API endpoint update

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -27,4 +27,44 @@ router.get("/", async (req, res) => {
   }
 });
 
+//belirli bir kategoriyi getirme (Read - Single)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+
+    try {
+      const category = await Category.findById(categoryId);
+
+      res.status(200).json(category);
+    } catch (error) {
+      res.status(404).json({ error: "Category not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//kategory guncelleme (Update)
+router.put("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const updates = req.body;
+
+    const existingCategory = await Category.findById(categoryId);
+    if(!existingCategory){
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      updates,
+      { new: true }
+    );
+
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;

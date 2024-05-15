cfe143484c0f2a91d7dbc37c095a18777c800038
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-16 00:37:02 +0300

    For the coupon model, create update delete search by id and code operations were performed for Postman.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const categoryRoute = require("./categories.js");
 const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
+const couponRoute = require("./coupons.js");
 
 //Her rotayi ilgili yol altinda kullaniyoruz
 router.use("/categories", categoryRoute);
 router.use("/auth", authRoute);
 router.use("/products", productRoute);
+router.use("/coupons", couponRoute);
 
 module.exports = router;

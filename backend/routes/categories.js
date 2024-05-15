e1f11fe7713b5b54312d8717dbf9be13fe4b7854
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-15 14:01:31 +0300

    Categories API endpoint

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category.js");
+
+//yeni bir kategori olusturma (create)
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body;
+
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Tüm kategorileri getirme (Read - All)
 router.get("/", async (req, res) => {
-  res.send("Kategoriler getirildi");
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
 module.exports = router;

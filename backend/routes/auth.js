ce7d1589c4a2b9f7dd70a1f6db649b8705de599c
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-15 19:54:00 +0300

    login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -37,4 +37,34 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//kullanici girisi
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid Email" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+
+    if (!isPasswordValid){
+      return res.status(401).json({ error: "Invalid Password" });
+    }
+    
+
+    res.status(200).json({
+      id: user._id,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      avatar: user.avatar,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;

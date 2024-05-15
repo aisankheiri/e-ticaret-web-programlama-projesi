e4773ffb7f82b39cbef1c34e45a6a74652963d89
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-15 16:40:21 +0300

    Bcryptjs for secure password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User.js");
 
 //kullanici olusturma (Create - Register)
@@ -7,10 +8,18 @@ const User = require("../models/User.js");
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json("Email adress is already registerd!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
 

2accdcd89c027eeef4cf3c638e9914791d0c15b9
aisankheiri <116971203+aisankheiri@users.noreply.github.com>
2024-05-15 16:16:42 +0300

    Register User

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 //Diger route dosyalarini ice aktariyoruz
-const productRoute = require("./products.js");
 const categoryRoute = require("./categories.js");
+const authRoute = require("./auth.js");
+const productRoute = require("./products.js");
 
 //Her rotayi ilgili yol altinda kullaniyoruz
-router.use("/categories",categoryRoute);
-router.use("/products",productRoute);
+router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
+router.use("/products", productRoute);
 
 module.exports = router;
